d69bc57c33b5244475ca93b300a518fcd53c1acf
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-03-14 02:08:25 -0700

    debugged functions.

--- blockchain.py
+++ blockchain.py
@@ -19,19 +19,19 @@ class Block(object):
 class Blockchain(object):
     """Implementatiion of the blockchain object."""
 
-    def __init__(self, genesis_block):
+    def __init__(self):
         """Initialize the blockchain."""
         self._chain = [create_genesis_block()]
 
 
 def calculate_hash(index, previous_hash, timestamp, data):
     """Generate a hash for a new block."""
-    return pwd_context.hash(index + previous_hash + timestamp + data)
+    return pwd_context.hash(str(index) + previous_hash + str(timestamp) + data)
 
 
-def generate_next_block(block_data):
+def generate_next_block(block_data, blockchain):
     """Generate a new block object."""
-    previous_block = get_previous_block()
+    previous_block = get_previous_block(blockchain)
     next_index = previous_block.index + 1
     next_timestamp = datetime.date.today()
     next_hash = calculate_hash(next_index, previous_block.block_hash, next_timestamp, block_data)
@@ -43,11 +43,11 @@ def create_genesis_block():
     return Block(0, "0", 1465154705, "the genesis block", "$6$rounds=656000$3B8jwjIaMBHvO4Rk$73YPhzB2ntaF7iwP6i7YChVMK4RD9Qp6rBuqMYmNYgyGtuMCV.NB.JXLNi29oYNyQJTRERWRac8hABzcd9lHO1")
 
 
-def add_block(blockchain, block):
+def add_block(block, blockchain):
     """Add a new block to the blockchain."""
     blockchain._chain.append(block)
 
 
 def get_previous_block(blockchain):
     """Return the latest block of the blockchain."""
-    return blockchain[-1]
+    return blockchain._chain[-1]
